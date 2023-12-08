932bfcecc485407c3253c0f27c618cb4d149b49c
EvgenijBaltais <evgenij.baltais@gmail.com>
2023-12-08 13:51:12 +0300

    pravki

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
 
@@ -9,5 +11,24 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name].bundle.js'
-	}
+	},
+	resolve: {
+		extensions: ['.js'],
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@core': path.resolve(__dirname, 'src', 'core')
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			path: './index.html'
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src', 'favicon.ico'), to: path.resolve(__dirname, 'dist')
+				}
+			],
+		}),
+	]
 }
